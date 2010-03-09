6e94a96b5c3dfe0d2a5b5d4342dde4a1e91802a2
Beau D. Simensen <beau@dflydev.com>
2010-03-08 23:00:16 -0800

    o Provide future support for site configuration.

--- application/config/dd_ci_vendorlibs.php
+++ application/config/dd_ci_vendorlibs.php
@@ -2,4 +2,9 @@
 /**
  * Placeholder for future configuration of the dd_ci_vendorlibs package.
  */
+
+// Include site-specific vendorlibs configuration if it exists.
+$siteConfig = APPPATH.'config/dd_ci_vendorlibs_site'.EXT;
+if ( file_exists($siteConfig) ) include($siteConfig);
+
 ?>
---
+++ application/config/dd_ci_vendorlibs_site.example.php
@@ -0,0 +1,5 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * Override application vendorlibs configuration specific for a site/deployment.
+ */
+?>
