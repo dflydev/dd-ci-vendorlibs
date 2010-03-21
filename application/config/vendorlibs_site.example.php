e4216d2a7c2e57d4e761e5e825ba44cdd13dcbc5
Beau D. Simensen <beau@dflydev.com>
2010-03-21 02:04:27 -0700

    o Documentation and enhanced functionality beyond simple scanning.

---
+++ application/config/vendorlibs_site.example.php
@@ -0,0 +1,20 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * Override application vendorlibs configuration specific for a site/deployment.
+ */
+
+/**
+ * Site paths
+ */
+//$config['vendorlibs_site_paths'][] = '/home/someuser/simplepie-trunk';
+//$config['vendorlibs_site_paths'][] = '/home/someuser/safsi-exp/lib';
+//$config['vendorlibs_site_paths'][] = '/home/someuser/repose-0.5c/lib';
+
+/**
+ * Individual path maps
+ */
+// $config['vendorlibs_map']['simplepie'] = '/home/someuser/simplepie-trunk';
+// $config['vendorlibs_map']['safsi'] = '/home/someuser/safsi-exp/lib';
+// $config['vendorlibs_map']['repose'] = '/home/someuser/repose-0.5c/lib';
+
+?>
